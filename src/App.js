6e284cf4e3d116484a3bb169b0cdd527c19ba515
pks <prakashsaini92@gmail.com>
2022-11-27 06:58:17 -0800

    api fetch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
 
 function App() {
+  const [data,setData] = useState([])
+  useEffect(() => {
+    const getData = async() => {
+        const result = await fetch("https://jsonplaceholder.typicode.com/users");
+        const response =  await result.json();
+        setData(response);
+    }
+    getData();
+  },[])
   return (
     <div className="App">
       <header className="App-header">
         <h1>Rahul SAINIIIII</h1>
-         <ul>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-         </ul>
+         <table>
+          <thead>
+         <tr>
+         <td>Name</td>
+          <td>email</td>
+         </tr>
+          </thead>
+          {data?.map((item) => <tr> 
+            <td>{item.name} </td>
+            <td>{item.email} </td>
+          </tr> )}
+         
+         </table>
       </header>
     </div>
   );
